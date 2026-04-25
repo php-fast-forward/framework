3e44955868f9b1a687f5e7d39b41102258815023
Felipe Sayão Lobato Abreu <github@mentordosnerds.com>
2026-04-24 21:38:50 -0300

    chore: ignore unused and shadow dependency findings

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,3 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
 /**
  * Fast Forward Framework — a lightweight PHP framework for building modern web applications with a simple and elegant API.
  *
@@ -11,3 +15,13 @@
  * @see      https://github.com/php-fast-forward/framework/issues
  * @see      https://datatracker.ietf.org/doc/html/rfc2119
  */
+
+use FastForward\DevTools\Config\ComposerDependencyAnalyserConfig;
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return ComposerDependencyAnalyserConfig::configure(
+    static function (Configuration $configuration): void {
+        $configuration->ignoreErrors([ErrorType::SHADOW_DEPENDENCY, ErrorType::UNUSED_DEPENDENCY]);
+    }
+);
